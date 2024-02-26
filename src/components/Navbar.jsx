6a460bead5b25e78b6ce3250233a6216a06b5b7c
Miranda Eriksson Thulstrup <Miranda.thulstrup@gmail.com>
2024-02-26 13:37:46 +0100

    started on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className="bg-[#f7f2e3]" >
-      <div className="flex ">
-        Nav
+    <header className="bg-[#f7f2e3]">
+      <div className="container mx-auto min-h-10 flex items-center p-4 justify-between">
+            <NavLink to="/">Logga</NavLink>
+        <div className="flex gap-8">
+             <NavLink to="/statistics">Statistics</NavLink>
+             <NavLink to="/bookshelf">Bookshelf</NavLink>
+             <NavLink to="/books">Find Books</NavLink>
+        </div>
+             <NavLink to="/profile">Profile</NavLink>
       </div>
-    </div>
+    </header>
   )
 }
 
