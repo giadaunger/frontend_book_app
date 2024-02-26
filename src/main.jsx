92ff4c3d8fbab8288aecc7f8e790bd299b0a2200
Giada Unger <giada.unger02@gmail.com>
2024-02-26 12:53:03 +0100

    Installed tailwind and react-router-dom + did the layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './index.css'
 
+import Layout from './pages/Layoute';
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
---
+++ src/pages/Layoute.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-auto">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
