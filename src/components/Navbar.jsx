0f3afc80cc79fed18acc100e04a868707d569135
Giada Unger <giada.unger02@gmail.com>
2024-02-27 15:01:13 +0100

    Added some stuling to the logged out navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function SignedIn() {
 
 function SignedOut() {
     return (
-        <header>
+        <header className="flex justify-center bg-[#f8f2e9] shadow-md mb-10">
             <div className="md:w-2/3 w-11/12 mx-auto flex justify-between items-center p-2">
                 <NavLink to="/">Logo</NavLink>
                 <div className="space-x-2">
