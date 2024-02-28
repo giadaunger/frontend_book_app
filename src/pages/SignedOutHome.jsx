3a6aa505003fe823df1b508190187319c11dc89f
Giada Unger <giada.unger02@gmail.com>
2024-02-28 10:10:04 +0100

    Started to fill ot the signedOutHome page

--- src/pages/SignedOutHome.jsx
+++ src/pages/SignedOutHome.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import gif1 from '../assets/44VQ.gif'
 
 function SignedOutHome() {
-  return (
-    <div>SignedOutHome</div>
-  )
+    return (
+        <div>
+            <div className="md:w-2/3 w-11/12 mx-auto bg-yellow-200">
+                <h2 className="text-center text-4xl mb-10">Welcome to StoryData</h2>
+                <h3 className="text-xl">Example 1</h3>
+                <div className="sm:flex mb-20">
+                    <img
+                        src={gif1}
+                        alt="gif1"
+                        className="max-h-80 sm:max-h-none sm:w-1/2 sm:mr-4"
+                    />
+                    <p className="sm:w-1/2 sm:flex-grow">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione quasi magni magnam officia libero adipisci voluptatibus vitae dolorum quaerat, incidunt sint numquam. Fugiat porro dolores eveniet libero iste cupiditate vero perspiciatis accusantium sapiente non autem voluptate, suscipit veritatis quibusdam, eius adipisci ducimus. Perspiciatis illum, odio iste libero vitae nesciunt quisquam aspernatur, ea optio eius, recusandae dicta blanditiis. Veniam ab vel nam numquam, nesciunt aspernatur quo, facere obcaecati adipisci, totam earum. Molestias odio non consequatur error expedita, sequi ea veritatis, iure deserunt laborum rerum, perspiciatis minus beatae? Dignissimos eius quisquam vel. Optio ipsum recusandae dolorem? Voluptates accusantium facere temporibus maxime rerum.</p>
+                </div>
+
+                <h3>Example 2</h3>
+                <div className="">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione quasi magni magnam officia libero adipisci voluptatibus vitae dolorum quaerat, incidunt sint numquam. Fugiat porro dolores eveniet libero iste cupiditate vero perspiciatis accusantium sapiente non autem voluptate, suscipit veritatis quibusdam, eius adipisci ducimus. Perspiciatis illum, odio iste libero vitae nesciunt quisquam aspernatur, ea optio eius, recusandae dicta blanditiis. Veniam ab vel nam numquam, nesciunt aspernatur quo, facere obcaecati adipisci, totam earum. Molestias odio non consequatur error expedita, sequi ea veritatis, iure deserunt laborum rerum, perspiciatis minus beatae? Dignissimos eius quisquam vel. Optio ipsum recusandae dolorem? Voluptates accusantium facere temporibus maxime rerum.</p>
+                    <img src={gif1} alt="gif2" />
+                </div>
+                <h3>Example 3</h3>
+                <div className="">
+                    <img src={gif1} alt="gif3" />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione quasi magni magnam officia libero adipisci voluptatibus vitae dolorum quaerat, incidunt sint numquam. Fugiat porro dolores eveniet libero iste cupiditate vero perspiciatis accusantium sapiente non autem voluptate, suscipit veritatis quibusdam, eius adipisci ducimus. Perspiciatis illum, odio iste libero vitae nesciunt quisquam aspernatur, ea optio eius, recusandae dicta blanditiis. Veniam ab vel nam numquam, nesciunt aspernatur quo, facere obcaecati adipisci, totam earum. Molestias odio non consequatur error expedita, sequi ea veritatis, iure deserunt laborum rerum, perspiciatis minus beatae? Dignissimos eius quisquam vel. Optio ipsum recusandae dolorem? Voluptates accusantium facere temporibus maxime rerum.</p>
+                </div>
+                <h3>Example 4</h3>
+                <div className="">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione quasi magni magnam officia libero adipisci voluptatibus vitae dolorum quaerat, incidunt sint numquam. Fugiat porro dolores eveniet libero iste cupiditate vero perspiciatis accusantium sapiente non autem voluptate, suscipit veritatis quibusdam, eius adipisci ducimus. Perspiciatis illum, odio iste libero vitae nesciunt quisquam aspernatur, ea optio eius, recusandae dicta blanditiis. Veniam ab vel nam numquam, nesciunt aspernatur quo, facere obcaecati adipisci, totam earum. Molestias odio non consequatur error expedita, sequi ea veritatis, iure deserunt laborum rerum, perspiciatis minus beatae? Dignissimos eius quisquam vel. Optio ipsum recusandae dolorem? Voluptates accusantium facere temporibus maxime rerum.</p>
+                    <img src={gif1} alt="gif4" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SignedOutHome
