934c7dd5a002888addfa4f5ae558db002bf5939f
Mirandaet <miranda.thulstrup@gmail.com>
2024-03-13 11:15:38 +0100

    changed sign in nav

--- src/cookies/cookies.js
+++ src/cookies/cookies.js
@@ -1,6 +1,13 @@
-export function setCookie(name, value, days) {
+export function setLongCookie(name, value, days) {
   const date = new Date();
   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+export function setCookie(name, value, minutes) {
+  const date = new Date();
+  date.setTime(date.getTime() + minutes * 60 * 1000);
   const expires = "expires=" + date.toUTCString();
   document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
