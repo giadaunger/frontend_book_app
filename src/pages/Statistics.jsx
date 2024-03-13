407a33a474e9185ad1a63474a327917b8fb0ab25
Giada Unger <giada.unger02@gmail.com>
2024-03-13 16:48:36 +0100

    Added the first chart regarding genres

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import useStore from '../store/ReadingBooksStore';
+import { useCookies } from 'react-cookie';
+import { PieChart } from '@mui/x-charts/PieChart';
 
 function Statistics() {
+  const [cookies] = useCookies(['user']);
+  const { setReadingBooks, readingBooks, fetchReadingBooks } = useStore();
+  const [genreCounts, setGenreCounts] = useState({});
+  const [genreColors, setGenreColors] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetchReadingBooks();
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    };
+    fetchData();
+  }, [fetchReadingBooks]);
+
+  useEffect(() => {
+    if (readingBooks) {
+      const counts = {};
+      const colors = {};
+      readingBooks.forEach(book => {
+        const genre = book.book.main_category.name;
+        counts[genre] = (counts[genre] || 0) + 1;
+        colors[genre] = book.book.main_category.color_code; // Assuming color_code is the property that holds the color
+      });
+      setGenreCounts(counts);
+      setGenreColors(colors);
+    }
+  }, [readingBooks]);
+
+  const pieChartGenreData = Object.entries(genreCounts).map(([label, value]) => ({
+    id: label,
+    value,
+    label,
+    color: genreColors[label],
+  }));
+
   return (
-    <div className="md:w-2/3 w-11/12 mx-auto bg-yellow-200 p-4">
-        <h1>Statistics</h1>
-        <h2>Most read genres</h2>
-        <div></div>
-        <h2>Read books per year</h2>
-        <h2>Read books - 6 months</h2>
-        <h2>Read books per month</h2>
-        <h2>Read page per year</h2>
-        <h2>Read pages - 6 months</h2>
-        <h2>Read pages per month</h2>
-        <h2>Read pages per week</h2>
-        <h2>Read pages per days</h2>
+    <div className="md:w-2/3 w-11/12 mx-auto">
+      <h2 className="text-4xl mb-10 text-center">Statistics</h2>
+      <h3 className="text-xl mb-5">Most read genres this year</h3>
+      <div className="">
+        <PieChart
+          series={[{ data: pieChartGenreData }]}
+          width={400}
+          height={200}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Statistics
+export default Statistics;
