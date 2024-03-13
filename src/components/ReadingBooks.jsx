9ff8afec7554eaa182699800e6db3d94f0f67604
Mirandaet <miranda.thulstrup@gmail.com>
2024-03-13 16:52:02 +0100

    made book svgs

--- src/components/ReadingBooks.jsx
+++ src/components/ReadingBooks.jsx
@@ -1,26 +1,71 @@
-import React, { useEffect } from 'react'
-import useStore from '../store/ReadingBooksStore'
-import { useCookies } from 'react-cookie';
+import React, { useEffect, useState } from "react";
+import useStore from "../store/ReadingBooksStore";
+import { useCookies } from "react-cookie";
+import bookOutline from "../assets/book_outline.svg";
+import book_filled from "../assets/book_filled.svg";
 
 function ReadingBooks() {
-  const [cookies] = useCookies(['user']);
   const { setReadingBooks, readingBooks, fetchReadingBooks } = useStore();
+  const [cookies] = useCookies(["user"]);
 
-  useEffect(() =>{
-    console.log(cookies.user)
-    const books = fetchReadingBooks()
-  },[])
-
+  useEffect(() => {
+    console.log(cookies.user);
+    const books = fetchReadingBooks();
+  }, []);
   return (
-    <div className='flex flex-col gap-5'>
-      { readingBooks &&
-      readingBooks.map((book)=> {
-        return <div className='bg-gray-500 min-w-60 min-h-60 border'>
-          <div className=''>{book.book.title}</div>
-          </div>
-      })}
+    <div className="flex flex-col gap-5">
+      {readingBooks &&
+        readingBooks.map((book) => {
+          return (
+            <div className="bg-gray-100 min-w-60 min-h-60 border flex p-10 gap-10">
+              <div>
+                <svg
+                  className={`fill-[${book.book.main_category.color_code}] absolute`}
+                  width="100" 
+                  height="150"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    width="60"
+                    height="120"
+                    x="5"
+                    y="5"
+                    rx="10"
+                    ry="10"
+                  />
+                </svg>
+                <svg
+                  className={`fill-[${book.book.main_category.color_code}] absolute stroke-white`}
+                  width="100"  
+                  height="150"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    width="60"
+                    height="120"
+                    x="5"
+                    y="5"
+                    rx="10"
+                    ry="10"
+                    strokeWidth={2}
+                  />
+                </svg>
+              </div>
+              {/* { book &&
+              <ProgressBar book={book}/>
+              } */}
+              <div className="mt-4 ml-10">
+                <div>{book.book.title}</div>
+                {console.log(book.book.main_category.color_code)}
+                <div className="pt-2">
+                  {book.pages_read} / {book.book.page_count} pages
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
-  )
+  );
 }
 
-export default ReadingBooks
+export default ReadingBooks;
--- src/store/ReadingBooksStore.js
+++ src/store/ReadingBooksStore.js
@@ -6,9 +6,9 @@ const useStore = create((set) => ({
   readingBooks: false,
   fetchReadingBooks: async () => {
     try {
-      const accessToken = getCookie("accessToken")
+      const accessToken = getCookie("accessToken");
       console.log("in fetch reading books");
-      console.log("token ", accessToken.access_token)
+      console.log("token ", accessToken.access_token);
       const response = await fetch(`http://127.0.0.1:8000/users/reading`, {
         method: "GET",
         headers: new Headers({
@@ -16,7 +16,6 @@ const useStore = create((set) => ({
         }),
       });
       if (!response.ok) {
-        console.log("Fail");
         throw new Error("Failed to fetch books");
       }
       const usersReadingBooks = await response.json();
@@ -27,7 +26,21 @@ const useStore = create((set) => ({
   },
   setReadingBooks: (usersReadingBooks) =>
     set({ readingBooks: usersReadingBooks }),
+  fetchCategories: async () => {
+    try {
+      const accessToken = getCookie("accessToken");
+      console.log("in fetch reading books");
+      console.log("token ", accessToken.access_token);
+      const response = await fetch(`http://127.0.0.1:8000/categories`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch categories");
+      }
+      const usersReadingBooks = await response.json();
+      set({ readingBooks: usersReadingBooks });
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  },
 }));
-
 
 export default useStore;
