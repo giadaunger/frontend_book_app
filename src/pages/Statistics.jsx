e49a85ccb6134fb1527023f32c4f36ca6b93ec70
Giada Unger <giada.unger02@gmail.com>
2024-03-17 07:18:56 +0100

    Worked on the stats some more

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -3,7 +3,6 @@ import useStore from '../store/ReadBooksStore';
 import { useCookies } from 'react-cookie';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { Chart } from "react-google-charts";
-import { BarChart } from '@mui/x-charts/BarChart';
 
 function Statistics() {
   const [cookies] = useCookies(['user']);
@@ -14,6 +13,7 @@ function Statistics() {
   const [finishedPerMonth, setFinishedPerMonth] = useState({});
   const [pagesReadPerMonth, setPagesReadPerMonth] = useState({});
   const [bookReadingDuration, setBookReadDuration] = useState({});
+  const [authors, setAuthors] = useState({})
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   useEffect(() => {
@@ -33,13 +33,16 @@ function Statistics() {
       const lastFiveBooks = readBooks
         .sort((a, b) => new Date(b.finished_date) - new Date(a.finished_date))
         .slice(0, 5);
+      const authorCounts = {};
       const counts = {};
       const colors = {};
       const startedReadingMonth = {};
       const finishedReadingMonth = {};
-      const readingDuration = {}
+      const readingDuration = {};
       const pagesRead = {};
       readBooks.forEach(book => {
+        const author = book.book.author;
+        console.log(book.book.authors.author);
         const genre = book.book.main_category.name;
         const startMonth = new Date(book.start_date).getMonth() + 1;
         const endMonth = new Date(book.finished_date).getMonth() + 1;
@@ -49,6 +52,7 @@ function Statistics() {
         counts[genre] = (counts[genre] || 0) + 1;
         colors[genre] = book.book.main_category.color_code;
         pagesRead[key] = (pagesRead[key] || 0) + book.pages_read;
+        authorCounts[author] = (authorCounts[author] || 0) + 1;
       });
       lastFiveBooks.forEach(book => {
         const startReadingDate = new Date(book.start_date);
@@ -57,16 +61,27 @@ function Statistics() {
         const readingDurationDays = Math.ceil(readingDurationMs / (1000 * 60 * 60 * 24));
         readingDuration[book.book.title] = readingDurationDays;
       });
+      const sortedAuthors = Object.entries(authorCounts).sort(([, countA], [, countB]) => countB - countA);
+      const topAuthors = sortedAuthors.slice(0, 5);
       setGenreCounts(counts);
       setGenreColors(colors);
       setReadPerMonth(startedReadingMonth);
       setFinishedPerMonth(finishedReadingMonth);
       setPagesReadPerMonth(pagesRead);
-      setBookReadDuration(readingDuration)
+      setBookReadDuration(readingDuration);
+      setAuthors(topAuthors);
     }
   }, [readBooks]);
 
+
   const pieChartGenreData = Object.entries(genreCounts).map(([label, value]) => ({
+    id: label,
+    value,
+    label,
+    color: genreColors[label],
+  }));
+
+  const pieChartAuthorData = Object.entries(authors).map(([label, value]) => ({
     id: label,
     value,
     label,
@@ -126,6 +141,14 @@ function Statistics() {
           width={800}
           height={400}
         />
+      </div>
+      <h3 className="text-xl mb-10 text-center">Most read authors this year</h3>
+      <div className="flex justify-start mb-20">
+        {/* <PieChart
+          series={[{ data: topAuthors }]}
+          width={800}
+          height={400}
+        /> */}
       </div>
       <h3 className="text-xl mb-10 text-center">Books started each month</h3>
       <div className="flex justify-start mb-20">
