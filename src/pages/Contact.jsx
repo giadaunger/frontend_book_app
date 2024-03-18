346156f1d5f94939cc1479f8fa096de612400b34
Giada Unger <giada.unger02@gmail.com>
2024-03-18 09:47:00 +0100

    Added some more styling to the form and the loggin button in the nav

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
                 <GoBackBtn />
             </div>
             <div className="container bg-[#f8f2e9] flex mx-auto rounded-md p-6 shadow-md">
-                <form className="mx-auto">
+                <form className="mx-auto lg:w-1/2 w-5/6">
                     <div className="flex mx-auto flex-col">
                         <img src={Logo} alt="logo" className="w-36 h-36 mx-auto" />
                         <label htmlFor="email">Email:</label>
@@ -25,7 +25,7 @@ function Contact() {
                         />
                         <label htmlFor="password">Question:</label>
                         <textarea 
-                            rows="4" cols="50" 
+                            rows="4" 
                             placeholder="Enter your question"
                             className="mb-4 border border-black rounded-md p-2" 
                             value={question}
