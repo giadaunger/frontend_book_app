863df19983d4533c72a5161407910f6100d3f606
Giada Unger <giada.unger02@gmail.com>
2024-03-19 10:20:50 +0100

    Changed the component name from SignedInHome to Dashboard, and added get user fetch

---
+++ src/store/GetUser.js
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+import { getCookie, setCookie } from "../cookies";
+import { useCookies } from 'react-cookie';
+
+const UserStore = create((set) => ({
+    user: false,
+    setUser: (chosenUser) => set({ user: chosenUser }),
+    fetchUser: async () => {
+        try {
+            const accessToken = getCookie("accessToken")
+            console.log("in fetch read books");
+            console.log("token ", accessToken.access_token)
+            const response = await fetch(`http://127.0.0.1:8000/user/me`, {
+                method: "GET",
+                headers: new Headers({
+                    Authorization: `Bearer ${accessToken.access_token}`,
+                }),
+            });
+
+            if (!response.ok) {
+                console.log("Fail");
+                throw new Error("Failed to fetch books");
+            }
+            const userInfo = await response.json();
+            set({ user: userInfo });
+        } catch (error) {
+            console.error("Error fetching user:", error);
+        }
+    },
+}));
+
+export default UserStore;
