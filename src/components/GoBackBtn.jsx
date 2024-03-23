3136093502d4ca6fc05e633e05cc2d6b01fdbdda
Giada Unger <giada.unger02@gmail.com>
2024-03-23 13:22:34 +0100

    Added some more styling to the go back btn and added a back do dashboard btn in myBooks file

--- src/components/GoBackBtn.jsx
+++ src/components/GoBackBtn.jsx
@@ -7,7 +7,7 @@ function GoBackBtn() {
     return (
         <button
             onClick={() => navigate(-1)}
-            className="flex bg-[#f7f2e3] py-2 px-4 rounded font-semibold mt-2 shadow-md transition duration-300 hover:scale-125" >
+            className="flex bg-[#f7f2e3] py-2 px-4 rounded-md border font-semibold mt-2 shadow-md transition duration-300 hover:scale-125 hover:bg-[#bde1ed] hover:border-[#71bfd9]" >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
             </svg>
