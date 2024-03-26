103fae91d6c01284ef75c72fea6782c616f0fd9f
Mirandaet <miranda.thulstrup@gmail.com>
2024-03-26 12:21:12 +0100

    added click outside functionality to hambnurger bar

--- src/components/SignedInNav.jsx
+++ src/components/SignedInNav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import Logo from '../assets/StoryDataLogo.png'
 import useStore from "../store/UserStore";
@@ -9,10 +9,25 @@ function SignedInNav() {
     const [isOpen, setIsOpen] = useState(false);
     const location = useLocation();
     const { setUser } = useStore();
+    const ref = useRef(null);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
+    
+    useEffect(() => {
+        const handleOutSideClick = (event) => {
+          if (!ref.current?.contains(event.target)) {
+            setIsOpen(false)
+          }
+        };
+    
+        window.addEventListener("mousedown", handleOutSideClick);
+    
+        return () => {
+          window.removeEventListener("mousedown", handleOutSideClick);
+        };
+      }, [ref]);    
 
     return (
         <header className="flex justify-center bg-[#f8f2e9] shadow-md mb-10">
@@ -23,9 +38,9 @@ function SignedInNav() {
                         alt="logo"
                         className="w-24 h-24" />
                 </NavLink>
-                <div className="ml-auto sm:ml-0 flex items-center text-black">
-                    <div className="sm:hidden">
-                        <button onClick={toggleMenu} className="focus:outline-none mr-5 mt-2">
+                <div ref={ref} className="ml-auto lg:ml-0 flex items-center text-black">
+                    <div className="lg:hidden">
+                        <button onClick={toggleMenu} className="focus:outline-none mr-5 mt-2 transition duration-300 hover:scale-110 hover:text-[#71bfd9]">
                             <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 {isOpen ? (
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
@@ -35,15 +50,15 @@ function SignedInNav() {
                             </svg>
                         </button>
                     </div>
-                    <div className={`sm:flex ${isOpen ? 'flex flex-col rounded-b-lg shadow-md sm:shadow-none absolute top-24 right-0 bg-[#f8f2e9] p-5 sm:flex-row sm:relative sm:top-0' : 'hidden sm:visible'} sm:items-center sm:gap-40`}>
-                        <div className="flex flex-col sm:flex-row">
-                            <NavLink to="/" className={`mr-5 transition duration-300 hover:scale-110 ${location.pathname === '/bookshelf' ? 'text-[#ff5277]' : ''}`}>Bookshelf</NavLink>
-                            <NavLink to="/statistics" className={`mr-5 transition duration-300 hover:scale-110 ${location.pathname === '/statistics' ? 'text-[#ff5277]' : ''}`}>Statistics</NavLink>
-                            <NavLink to="/findbooks" className={`mr-5 transition duration-300 hover:scale-110 ${location.pathname === '/findbooks' ? 'text-[#ff5277]' : ''}`}>Find Books</NavLink>
-                            <NavLink to="/mybooks" className={`mr-5 transition duration-300 hover:scale-110 ${location.pathname === '/mybooks' ? 'text-[#ff5277]' : ''}`}>My Books</NavLink>
+                    <div className={`lg:flex ${isOpen ? 'flex flex-col rounded-b-lg shadow-md lg:shadow-none absolute top-24 md:w-[30%] lg:w-full right-0 bg-[#f8f2e9] p-5 lg:flex-row lg:relative lg:top-0' : 'hidden lg:visible'} lg:items-center lg:gap-40`}>
+                        <div className="flex flex-col lg:flex-row lg:w-full">
+                            <NavLink to="/" className={`mr-5 transition duration-300 hover:scale-110 hover:text-[#71bfd9] ${location.pathname === '/' ? 'text-[#7ad56a]' : ''}`}>Bookshelf</NavLink>
+                            <NavLink to="/statistics" className={`mr-5 transition duration-300 hover:scale-110 hover:text-[#71bfd9] ${location.pathname === '/statistics' ? 'text-[#7ad56a]' : ''}`}>Statistics</NavLink>
+                            <NavLink to="/findbooks" className={`mr-5 transition duration-300 hover:scale-110 hover:text-[#71bfd9] ${location.pathname === '/findbooks' ? 'text-[#7ad56a]' : ''}`}>Find Books</NavLink>
+                            <NavLink to="/mybooks" className={`mr-5 mb-5 lg:mb-0 transition duration-300 hover:scale-110 hover:text-[#71bfd9] ${location.pathname === '/mybooks' ? 'text-[#7ad56a]' : ''}`}>My Books</NavLink>
                             <button
                                 onClick={() => deleteCookie(["accessToken"])}
-                                className="transition duration-300 hover:scale-110 border border-black rounded-md p-1" >
+                                className="transition duration-300 hover:scale-110 border border-black rounded-md p-1 hover:bg-[#bde1ed] hover:border-[#71bfd9]" >
                                 Logout
                             </button>
                         </div>
@@ -51,7 +66,7 @@ function SignedInNav() {
                     </div>
                 </div>
                 <div className="flex">
-                    <NavLink to="/profile" className={`mr-5 transition duration-300 hover:scale-110 ${location.pathname === '/profile' ? 'text-[#ff5277]' : ''}`}>
+                    <NavLink to="/profile" className={`mr-5 transition duration-300 hover:scale-110 hover:text-[#71bfd9] ${location.pathname === '/profile' ? 'text-[#7ad56a]' : ''}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
