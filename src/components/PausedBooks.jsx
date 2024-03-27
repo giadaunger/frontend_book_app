18c363542acc9b2d3f9dd29ee11d36209680e258
Mirandaet <miranda.thulstrup@gmail.com>
2024-03-27 17:41:39 +0100

    added pause, unpause update pages and finished date to my books

--- src/components/PausedBooks.jsx
+++ src/components/PausedBooks.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import ReadingBooksStore from '../store/ReadingBooksStore'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import ReadingBooksStore from "../store/ReadingBooksStore";
+import { NavLink } from "react-router-dom";
+import UpdateBookVersionStore from "../store/UpdateBookVersionStore";
 
 function PausedBooks() {
-    const {readingBooks} = ReadingBooksStore()
+  const { readingBooks, fetchReadingBooks } = ReadingBooksStore();
+  const { putUnpaused } = UpdateBookVersionStore();
+
+  async function unpauseBook(book) {
+    const shelf = readingBooks.find(
+      (el) => el.id == book.id
+    )
+    const response = await putUnpaused(shelf.book_version_id);
+    try {
+      if (response === true) {
+        const res = await fetchReadingBooks();
+      }
+    } catch (error) {
+      console.error("Error during update:", error);
+    }
+  }
+
   return (
-    <div>{readingBooks &&
-        readingBooks.filter((el) => el.paused === true).map(book =>{
-          return(
-              <NavLink to={`/bookpage/${book.book_version.book.id}`}>
+    <div>
+      {readingBooks &&
+        readingBooks
+          .filter((el) => el.paused === true)
+          .map((book) => {
+            return (
+              <div className="flex gap-4">
+                <NavLink to={`/bookpage/${book.book_version.book.id}`}>
                   <img src={book.book_version.book_cover.url} alt="" />
-                  {book.book_version.book.title}
-              </NavLink>
-          )
-        })}</div>
-  )
+                </NavLink>
+                <div className="flex flex-col gap-2 items-center mt-2">
+                    {book.book_version.book.title}
+                    <button
+                      onClick={() => unpauseBook(book)}
+                      className="text-sm bg-white px-2 py-1 rounded-full mt-2 shadow-md"
+                    >
+                      Unpause book
+                    </button>
+                </div>
+              </div>
+            );
+          })}
+    </div>
+  );
 }
 
-export default PausedBooks
+export default PausedBooks;
--- src/components/ReadBooks.jsx
+++ src/components/ReadBooks.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import ReadBooksStore from '../store/ReadBooksStore'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import ReadBooksStore from "../store/ReadBooksStore";
+import { NavLink } from "react-router-dom";
 
 function ReadBooks() {
-    const {readBooks} = ReadBooksStore()
+  const { readBooks } = ReadBooksStore();
   return (
-    <div>{readBooks &&
-        readBooks.map(book =>{
-          return(
+    <div>
+      {readBooks &&
+        readBooks.map((book) => {
+          return (
+            <div className="flex gap-4">
               <NavLink to={`/bookpage/${book.book_version.book.id}`}>
-                  <img src={book.book_version.book_cover.url} alt="" />
-                  {book.book_version.book.title}
+                <img src={book.book_version.book_cover.url} alt="" />
               </NavLink>
-          )
-        })}</div>
-
-  )
+              <div className="flex flex-col gap-2 mt-2">
+                <div>{book.book_version.book.title}</div>
+                <div>Finished: {book.finished_date.slice(0,10)}</div>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
-export default ReadBooks
+export default ReadBooks;
